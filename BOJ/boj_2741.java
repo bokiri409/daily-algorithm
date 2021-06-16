40ab1708dbc27eb3b24e6fbd0ac7da97e54da116
bokiri <bokiri16@gmail.com>
2021-06-16 22:37:39 +0900

    2021.06.16 daily algorithm

---
+++ BOJ/boj_2741.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+/*
+ * 2021.06.16 daily algo/commit
+ */
+
+import java.util.Scanner;
+
+public class boj_2741 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		for(int n=1; n<=N; n++) {
+			System.out.println(n);
+		}
+		sc.close();
+	}
+
+}
