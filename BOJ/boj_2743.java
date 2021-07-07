1c190121f0ad80e7b0d720ea597ba0d211a2dab2
bokiri <bokiri16@gmail.com>
2021-07-07 10:14:41 +0900

    2021.07.07 daily algorithm

---
+++ BOJ/boj_2743.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 2021.07.07 daily algo/commit
+ */
+
+public class boj_2743 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
+		
+		System.out.print(word.length());
+		
+		br.close();
+	}
+
+}
